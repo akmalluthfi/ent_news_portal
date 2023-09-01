0c223a345f5e8e3759b9fdc5ca09bae2769c1a5e
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-01 17:33:53 +0700

    add navbar dropdown

--- components/navbar.php
+++ components/navbar.php
@@ -2,8 +2,25 @@
     <div class="container">
         <a class="navbar-brand" href="/">News Portal</a>
 
-        <form class="d-flex" method="post" action="/logout">
-            <button class="btn btn-link text-danger" type="submit" name="logout">Logout</button>
-        </form>
+        <?php if (isset($_SESSION["login"])) : ?>
+            <div class="dropdown">
+                <button class="btn btn-link text-decoration-none text-black dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Hello, Username
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/dashboard/">Dashboard</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <form class="dropdown-item" method="post" action="/logout">
+                            <button class="btn btn-link text-danger p-0 m-0" type="submit" name="logout">Logout</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        <?php else : ?>
+            <a href="/login" class="btn btn-primary">Login</a>
+        <?php endif; ?>
     </div>
 </nav>
