1d5fb276ef4693dc8ba44dec88a3dc55016b58e7
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-01 13:39:42 +0700

    create feature logout

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,9 @@
 <nav class="navbar bg-body-tertiary shadow-sm mb-3">
     <div class="container">
         <a class="navbar-brand" href="/index.php">News Portal</a>
+
+        <form class="d-flex" method="post" action="/logout.php">
+            <button class="btn btn-link text-danger" type="submit" name="logout">Logout</button>
+        </form>
     </div>
 </nav>
