0106b5316532935a50945fff9e97bcaaf73f00bf
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-03 10:29:42 +0700

    fix error in dashboard/posts/show

--- dashboard/posts/show.php
+++ dashboard/posts/show.php
@@ -1,5 +1,5 @@
 <?php
-require '../functions.php';
+require './functions.php';
 
 authenticatedMiddleware();
 
@@ -38,23 +38,23 @@ if (empty($posts)) {
 
 ?>
 
-<?php require '../components/header.php' ?>
+<?php require '../../components/header.php' ?>
 
-<?php require '../components/navbar.php' ?>
+<?php require '../../components/navbar.php' ?>
 
 <div class="container">
 
     <div class="mb-5">
         <h1 class="fs-5 mb-3"><?= $post['title'] ?></h1>
 
-        <img src="../images/<?= $post['image'] ?>" alt="<?= $post['title'] ?> Image" class="rounded mb-3" style="width: 18rem;">
+        <img src="/images/<?= $post['image'] ?>" alt="<?= $post['title'] ?> Image" class="rounded mb-3" style="width: 18rem;">
 
         <p><?= $post['body']; ?></p>
     </div>
 
     <div class="d-flex justify-content-end mb-4">
-        <a href="/dashboard/" role="button" class="btn btn-secondary mb-3">Back</a>
+        <a href="/dashboard/posts" role="button" class="btn btn-secondary mb-3">Back</a>
     </div>
 </div>
 
-<?php require '../components/footer.php' ?>
+<?php require '../../components/footer.php' ?>
