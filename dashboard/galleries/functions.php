c94d40b02818387103bc518e93166cb8b8297142
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-03 11:08:27 +0700

    create function for images

---
+++ dashboard/galleries/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'ent_news-portal');
+
+
+function getImages()
+{
+}
--- dashboard/galleries/index.php
+++ dashboard/galleries/index.php
@@ -1,3 +1,13 @@
+<?php
+require './functions.php';
+
+authenticatedMiddleware();
+
+$userId = $_SESSION['user_id'];
+$posts = getPosts("SELECT * FROM posts WHERE user_id = $userId");
+
+?>
+
 <?php require '../../components/header.php' ?>
 
 <?php require '../../components/navbar.php' ?>
